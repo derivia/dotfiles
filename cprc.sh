06296d11c6fc786a70a54484c947663729704b10
derivia <contato.lorransoares@gmail.com>
2024-12-05 01:29:07 -0300

    feat: updated cprc

--- cprc.sh
+++ cprc.sh
@@ -7,6 +7,7 @@ TMUXSTAT="$(cmp --silent "./tmux.conf" "$HOME/.tmux.conf"; echo $?)"
 GCSTAT="$(cmp --silent "./gitconfig" "$HOME/.gitconfig"; echo $?)"
 I3STAT="$(cmp --silent "./i3/config" "$HOME/.config/i3/config"; echo $?)"
 I3BARSTAT="$(cmp --silent "./i3/i3status/config" "$HOME/.config/i3status/config"; echo $?)"
+I3BLKSTAT="$(cmp --silent "./i3/i3blocks/config" "$HOME/.config/i3blocks/config"; echo $?)"
 ALACSTAT="$(cmp --silent "./alacritty/alacritty.toml" "$HOME/.config/alacritty/alacritty.toml"; echo $?)"
 DUNSTSTAT="$(cmp --silent "./dunst/dunstrc" "$HOME/.config/dunst/dunstrc"; echo $?)"
 
@@ -38,6 +39,12 @@ fi
 if [[ $I3BARSTAT -ne 0 ]]; then
   cp "$HOME/.config/i3status/config" ./i3/i3status/config
   echo "i3bar config updated"
+fi
+
+if [[ $I3BLKSTAT -ne 0 ]]; then
+  rm -rf ./i3/i3blocks
+  cp -r "$HOME/.config/i3blocks" ./i3
+  echo "i3blocks updated"
 fi
 
 if [[ $ALACSTAT -ne 0 ]]; then
