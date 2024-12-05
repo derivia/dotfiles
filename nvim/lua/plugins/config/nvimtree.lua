7b9732cc6012ad8a3e4e92ff05ef75af75a07023
derivia <contato.lorransoares@gmail.com>
2024-12-05 01:29:24 -0300

    feat: faster lualine & nvimtree cwd tweaks

--- nvim/lua/plugins/config/nvimtree.lua
+++ nvim/lua/plugins/config/nvimtree.lua
@@ -15,16 +15,17 @@ local function own_on_attach(bufnr)
 	vim.keymap.set("n", "<C-t>", api.tree.change_root_to_node, opts("CD"))
 	vim.keymap.set("n", "<leader><C-l>", api.node.open.vertical, opts("Open: Vertical Split"))
 	vim.keymap.set("n", "<leader><C-j>", api.node.open.horizontal, opts("Open: Horizontal Split"))
+	vim.keymap.set("n", "s", api.marks.toggle, opts("Select node"))
 end
 
 plugin.setup({
-	update_cwd = true,
+	update_cwd = false,
 	update_focused_file = {
-		enable = true,
+		enable = false,
 		update_cwd = false,
 	},
 	on_attach = own_on_attach,
-	select_prompts = true,
+	select_prompts = false,
 	git = {
 		enable = false,
 	},
