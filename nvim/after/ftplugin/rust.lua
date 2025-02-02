eae3a8f2623a78160d9456a34ba4248b1e65020d
derivia <contato.lorransoares@gmail.com>
2025-02-01 22:15:02 -0300

    chore: removed unused file

--- nvim/after/ftplugin/rust.lua
+++
@@ -1,7 +0,0 @@
--- local bufnr = vim.api.nvim_get_current_buf()
--- vim.keymap.set("n", "<leader>fe", function()
--- 	vim.cmd.RustLsp("explainError")
--- end, { silent = true, buffer = bufnr })
--- vim.keymap.set("n", "<leader>cr", function()
--- 	vim.cmd.RustLsp("reloadWorkspace")
--- end, { silent = true, buffer = bufnr })
