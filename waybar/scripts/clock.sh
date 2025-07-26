6197d53aeb905b5c8d6050dbf8fed980e9c86b0b
derivia <contato.lorransoares@gmail.com>
2025-07-26 11:26:38 -0300

    feat: custom pt-br clock for waybar

---
+++ waybar/scripts/clock.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+LANG=pt_BR.UTF-8 date +"%A - %d de %B de %Y | %H:%M"
--- waybar/scripts/vpn.sh
+++ waybar/scripts/vpn.sh
@@ -5,15 +5,21 @@ GREEN="#a8e6cf"
 RED="#ff8b94"
 
 if ip addr show tun0 2>/dev/null | grep -q "state UP"; then
-    TUN_STATUS="<span foreground='$GREEN'>tun0: UP</span>"
+    TUN_STATUS="<span foreground='$GREEN'>tun0</span>"
 else
-    TUN_STATUS="<span foreground='$RED'>tun0: DOWN</span>"
+    TUN_STATUS="<span foreground='$RED'>tun0</span>"
+fi
+
+if ip addr show suica 2>&1 | grep -qv "does not exist"; then
+    PROTON_STATUS="<span foreground='$GREEN'>proton</span>"
+else
+    PROTON_STATUS="<span foreground='$RED'>proton</span>"
 fi
 
 if ip addr show ppp0 2>&1 | grep -qv "does not exist"; then
-    PPP_STATUS="<span foreground='$GREEN'>ppp0: UP</span>"
+    PPP_STATUS="<span foreground='$GREEN'>ppp0</span>"
 else
-    PPP_STATUS="<span foreground='$RED'>ppp0: DOWN</span>"
+    PPP_STATUS="<span foreground='$RED'>ppp0</span>"
 fi
 
-echo "$TUN_STATUS | $PPP_STATUS"
+echo "$TUN_STATUS | $PROTON_STATUS | $PPP_STATUS"
